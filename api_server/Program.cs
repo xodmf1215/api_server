bb49d27b1865b1cc96af8bdd66988c4ac521fcbb
xodmf1215 <xodmf1215@users.noreply.github.com>
2023-07-24 23:15:41 +0900

    Initial commit

---
+++ api_server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Build.Evaluation;
+namespace api_server
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            
+            Project p = new Project();
+            for(int i=0;i<10;i++)
+            {
+                i=i+1;
+            }
+        }
+    }
+}
